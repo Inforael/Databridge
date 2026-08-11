6ba42cfd987f31c681a836677fc6665f3f2670a1
Rael <inforael@gmail.com>
2026-08-10 21:32:50 -0300

    Add initial Cmd and Jogo files for Python commands and game setup

---
+++ Cmd.md
@@ -0,0 +1,22 @@
+09/08/2026  cmd 
+
+# Cmd Python
+## Cmd para ver a versão python
+
+### python --version 
+
+------------------------------------------------------
+
+cmd 
+
+python.exe -m pip install --upgrade pip
+
+-----------------------------
+pip install pygame
+python -m pip install pygame-ce
+
+---------------------------------
+cmd para ver pacotes instalados 
+pip freeze
+
+--------------------------------
---
+++ Jogo.py
@@ -0,0 +1,37 @@
+import pygame 
+
+# inicializar o pygame
+
+pygame.init()
+
+tamanho_tela = (800, 800)
+tela = pygame.display.set_mode(tamanho_tela)
+pygame.display.set_caption("Jogo Pegar Bolas")
+
+tamanho_bola = 15
+bola = pygame.rect(100, 500, tamanho_bola, tamanho_bola)
+tamanho_jogador = 100
+jogador = pygame.rect(0, 750, tamanho_jogador, 15)
+
+qtd_blocos_linha = 8
+qtd_linhas_blocos = 5
+qtd_total_blocos = qtd_blocos_linha * qtd_linhas_blocos
+
+def criar_blocos():
+    blocos = []
+
+    return blocos
+
+cores = {"branca": (255, 0, 0), 
+         1: (0, 255, 0), 
+         2: (0, 0, 255)
+         }
+
+
+
+
+
+
+
+
+
